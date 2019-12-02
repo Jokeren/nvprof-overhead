709f24dc652455ca905d9b487f72f32042454d08
Keren Zhou <robinho364@gmail.com>
2019-12-02 15:12:31 -0600

    Update run_nvprof.sh

--- run_nvprof.sh
+++ run_nvprof.sh
@@ -1,4 +1,6 @@
 LAGHOS_DIR=build/Laghos/cuda
 
+export CUPTI_PERFWORKS_SAMPLER=1
 time mpirun -np 1 nvprof -a pc_sampling --pc-sampling-period 5 --export-profile tmp.nvvp ${LAGHOS_DIR}/laghos -p 0 -m ${LAGHOS_DIR}/../data/square01_quad.mesh -rs 1 -tf 0.05 -pa
 rm tmp.nvvp
+unset CUPTI_PERFWORKS_SAMPLER
